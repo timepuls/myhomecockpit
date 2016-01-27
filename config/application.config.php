ca1a2a76f968b4c8f8b00fe965e0c360a7a0d830
timepuls <markus.wilhelm@timepuls.ch>
2016-01-27 16:52:26 +0100

    list of buildings

--- config/application.config.php
+++ config/application.config.php
@@ -8,7 +8,7 @@
 return array(
     // This should be an array of module namespaces used in the application.
     'modules' => array(
-        'Application',
+        'Application','Building',
     ),
 
     // These are various options for the listeners attached to the ModuleManager
@@ -19,7 +19,7 @@ return array(
         // Module class.
         'module_paths' => array(
             './module',
-            './vendor',
+            './vendor','./module',
         ),
 
         // An array of paths from which to glob configuration files after
--- config/autoload/global.php
+++ config/autoload/global.php
@@ -12,5 +12,17 @@
  */
 
 return array(
-    // ...
-);
+     'service_manager' => array(
+         'factories' => array(
+             'Zend\Db\Adapter\Adapter' =>
+                 'Zend\Db\Adapter\AdapterServiceFactory',
+         ),
+     ),
+     'db' => array(
+         'driver' => 'Pdo',
+         'dsn' => 'mysql:dbname=myhomecockpit;hostname=localhost',
+         'driver_options' => array(
+             PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
+         ),
+     ),
+ );
